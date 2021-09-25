449d01c3f7c18af20769a7040846cb34d50d6889
BIEN HUYNH CAO <1512031@student.hcmus.edu.vn>
2021-09-25 23:31:41 +0700

    update predict result 9/25/2021 23:31:30

--- js/64s-3d-1.js
+++ js/64s-3d-1.js
@@ -87,7 +87,7 @@ function parity_classification(num2, num1) {
     }
 }
 
-d3.json("https://raw.githubusercontent.com/bienhuynh/XSMBView/main/data/resultlotterytable.json", function (error, data) {
+d3.json("https://raw.githubusercontent.com/bienhuynh/XSMBView/main/data/resultlotterytable_09_2021.json", function (error, data) {
     if (error)
         throw error;
     var _64s = [];

27773c5630cb20f749ecdc7d4c08b9e5dfcb80e9
BIEN HUYNH CAO <1512031@student.hcmus.edu.vn>
2021-10-10 12:51:36 +0700

    update predict result 10/10/2021 12:51:24

--- js/36s_2.js
+++ js/36s_2.js
@@ -638,7 +638,7 @@ $(document).on('click', '.3d-g00', function () {
     var c4 = [strtmp[0], strtmp[1], strtmp[2], strtmp[3]];
     var _64s = getc4(c4);
     var _36s = get36s(_64s);
-    _64s.forEach(function (s, idx1) {
+    _36s.forEach(function (s, idx1) {
         _3ds.forEach(function (c, idx2) {
             result.push(c.toString() + s.toString());
         });
@@ -654,7 +654,8 @@ $(document).on('click', '.3d-g11', function () {
     var strtmp = $(this).attr('data-c4').split(",");
     var c4 = [strtmp[0], strtmp[1], strtmp[2], strtmp[3]];
     var _64s = getc4(c4);
-    _64s.forEach(function (s, idx1) {
+    var _36s = get36s(_64s);
+    _36s.forEach(function (s, idx1) {
         _3ds.forEach(function (c, idx2) {
             result.push(c.toString() + s.toString());
         });

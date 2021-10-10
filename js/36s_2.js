8b71126af8967733bda94b293887bb9f1f78cdee
BIEN HUYNH CAO <1512031@student.hcmus.edu.vn>
2021-10-10 12:36:07 +0700

    update predict result 10/10/2021 12:35:55

--- js/36s_2.js
+++ js/36s_2.js
@@ -96,7 +96,7 @@ function get36s(_64s) {
 
     var _36stmp = [];
     for (var l = 0; l < 100; l++) {
-        if (!_64s.includes(l < 10 ? '0' + l : l.toString())) _36stmp.push()
+        if (!_64s.includes(l < 10 ? '0' + l : l.toString())) _36stmp.push();
     }
     return _36stmp;
 }
